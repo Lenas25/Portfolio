0828d6a84799d095528c6b404837acfa76a699c3
Lenas25 <102402197+Lenas25@users.noreply.github.com>
2025-03-20 08:49:35 -0500

    🧪 testing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["tsx", "ts", "jsx", "js"],
+  compress: true, // Esto habilita la compresión en Next.js
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
