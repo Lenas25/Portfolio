3f78625e03ed7737b9a96484ccbe47724be6602a
Lenas25 <102402197+Lenas25@users.noreply.github.com>
2025-03-23 22:31:56 -0500

    inicio

--- components/footertype1.tsx
+++ components/footertype1.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Routes from "@/constants/routes";
+import Lottie from "lottie-react";
 import Link from "next/link";
 import {
   FaDownload,
@@ -8,8 +9,20 @@ import {
   FaInstagram,
   FaLinkedinIn,
 } from "react-icons/fa";
+import animationData from "@/public/lottiescrollup.json";
+import { useLottie } from "lottie-react";
 
 export const FooterType1 = () => {
+
+  const defaultOptions = {
+    animationData: animationData,
+    loop: true,
+    autoplay: true,
+  };
+
+  const { View } = useLottie(defaultOptions);
+
+
   return (
     <footer className="px-5 py-10 text-center md:w-[70%] z-10 mx-auto mt-5">
       <h5 className="text-2xl font-semibold md:text-4xl">LenasDev</h5>
@@ -55,6 +68,7 @@ export const FooterType1 = () => {
         <Link
           href={Routes.inicio}
           className="relative w-6 right-0 -top-44 block">
+            {View}
         </Link>
       </div>
     </footer>
