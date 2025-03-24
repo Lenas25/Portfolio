281e1d0f2f3f5e528e529d102aec780baf2391a2
Lenas25 <102402197+Lenas25@users.noreply.github.com>
2025-03-23 22:24:53 -0500

    inicio

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import { FooterType1 } from "@/components";
-import { Inicio } from "@/components/inicio/Inicio";
-
 export default function Home() {
   return (
     <div className="overflow-x-hidden h-full">
-      {/* <Inicio /> */}
-      <FooterType1 />
+      Hola
     </div>
   );
 }
